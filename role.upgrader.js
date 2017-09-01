bc896f8e5fe2d6a98e8136463b2861a57d93b05e
Vitt0ri0 <vityaii@mail.ru>
2017-09-02 02:06:53 +0300

    .

---
+++ role.upgrader.js
@@ -0,0 +1,18 @@
+var roleUpgrader = {
+
+    run: function(creep) {
+        if (creep.carry.energy == 0) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[1] == ERR_NOT_IN_RANGE)) {
+                creep.moveTo(sources[1]);
+            }
+        }
+        else {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+    }
+};
+
+module.exports = roleUpgrader;
