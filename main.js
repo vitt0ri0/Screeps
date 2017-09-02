0df2169cc039499afe58d5870ccb7c8329c82d54
Vitt0ri0 <vityaii@mail.ru>
2017-09-02 22:17:06 +0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
 module.exports.loop = function () {
     // Your code goes here
@@ -8,22 +9,22 @@ module.exports.loop = function () {
             roleHarvester.run(creep);
         }
     }
-    var maxHarvesters = 8;
-    // var currentNumHarvesters = _.sum(Game.creeps, (c) = > c.memory.role == 'harvester');
-    var currentNumHarvesters = Game.creeps.length;
-    console.log(currentNumHarvesters);
-
-    var name = undefined;
-    if (currentNumHarvesters < maxHarvesters) {
-        name = Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], undefined, {
-            role: 'harvester',
-            working: false
-        });
-    }
-
-    if (!(name < 0)) {
-        console.log("! new creep: " + name);
-    }
+    // var maxHarvesters = 8;
+    // // var currentNumHarvesters = _.sum(Game.creeps, (c) = > c.memory.role == 'harvester');
+    // var currentNumHarvesters = Game.creeps.length;
+    // console.log(currentNumHarvesters);
+    //
+    // var name = undefined;
+    // if (currentNumHarvesters < maxHarvesters) {
+    //     name = Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], undefined, {
+    //         role: 'harvester',
+    //         working: false
+    //     });
+    // }
+    //
+    // if (!(name < 0)) {
+    //     console.log("! new creep: " + name);
+    // }
 
 };
 
