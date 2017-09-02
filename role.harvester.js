fa105793330a78aff5f7948d7a87d1d3f4ba93be
Vitt0ri0 <vityaii@mail.ru>
2017-09-02 23:13:30 +0300

    .

--- role.harvester.js
+++ role.harvester.js
@@ -3,17 +3,17 @@ var roleHarvester = {
     /** @param {Creep} creep **/
     run: function (creep) {
         if (creep.carry.energy < creep.carryCapacity) {
-            if (creep.carry.energy == 0) {
-                creep.say('gonna harvest');
-            }
+            // if (creep.carry.energy == 0) {
+            //     creep.say('gonna harvest');
+            // }
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
             }
         } else {
-            if (creep.carry.energy == creep.carryCapacity) {
-                creep.say('carry energy');
-            }
+            // if (creep.carry.energy == creep.carryCapacity) {
+            //     creep.say('carry energy');
+            // }
             if (Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
                 if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(Game.spawns['Spawn1']);
--- role.upgrader.js
+++ role.upgrader.js
@@ -2,18 +2,18 @@ var roleUpgrader = {
 
         run: function (creep) {
             if (creep.carry.energy < creep.carryCapacity) {
-                if (creep.carry.energy == 0) {
-                    creep.say('gonna harvest');
-                }
+                // if (creep.carry.energy == 0) {
+                //     creep.say('gonna harvest');
+                // }
                 var sources = creep.room.find(FIND_SOURCES);
                 if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(sources[0]);
                     creep.say(creep.role + ' harvest');
                 }
             } else {
-                if (creep.carry.energy == creep.carryCapacity) {
-                    creep.say('gonna upgrade');
-                }
+                // if (creep.carry.energy == creep.carryCapacity) {
+                //     creep.say('gonna upgrade');
+                // }
                 if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(creep.room.controller);
                     creep.say(creep.role + ' upgrade controller');
