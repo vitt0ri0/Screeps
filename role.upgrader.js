1212e2088adbaebde98c99556d791b84b0b17e31
Vitt0ri0 <vityaii@mail.ru>
2017-09-03 21:21:05 +0300

    .

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,20 +1,25 @@
 var roleUpgrader = {
 
         run: function (creep) {
-            if (creep.carry.energy == 0) {
+            if (creep.memory.upgrading && creep.carry.energy == 0) {
+                creep.memory.upgrading = true;
+            } else if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+                creep.memory.upgrading = false;
+            }
                 // if (creep.carry.energy == 0) {
                 //     creep.say('gonna harvest');
                 // }
-                var sources = creep.room.find(FIND_SOURCES);
-                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
+            if (creep.memory.upgrading) {
+                if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller);
                 }
             } else {
                 // if (creep.carry.energy == creep.carryCapacity) {
                 //     creep.say('gonna upgrade');
                 // }
-                if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller);
+                var sources = creep.room.find(FIND_SOURCES);
+                if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
             }
         }
