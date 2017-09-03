316553eb7009aaf0d573f76d093a1f166b289b0b
Vitt0ri0 <vityaii@mail.ru>
2017-09-03 21:12:19 +0300

    .

--- main.js
+++ main.js
@@ -26,36 +26,33 @@ module.exports.loop = function () {
             numUpgraders += 1;
         }
     }
-    console.log(numHarvesters);
-    console.log(numUpgraders);
 
     // spawn new creeps
     if (numHarvesters < numUpgraders) {
         if (numHarvesters < maxHarversters) {
             Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], undefined, {role: 'harvester'});
         }
-    } else {
-        if (numUpgraders < maxUpgraders) {
-            Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE, MOVE], undefined, {role: 'upgrader'});
-        }
+    } else if (numUpgraders < maxUpgraders) {
+        Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE, MOVE], undefined, {role: 'upgrader'});
     }
 
 
-    // // var currentNumHarvesters = _.sum(Game.creeps, (c) = > c.memory.role == 'harvester');
-    // var currentNumHarvesters = Game.creeps.length;
-    // console.log(currentNumHarvesters);
-    //
-    // var name = undefined;
-    // if (currentNumHarvesters < maxHarvesters) {
-    //     name = Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], undefined, {
-    //         role: 'harvester',
-    //         working: false
-    //     });
-    // }
-    //
-    // if (!(name < 0)) {
-    //     console.log("! new creep: " + name);
-    // }
+// // var currentNumHarvesters = _.sum(Game.creeps, (c) = > c.memory.role == 'harvester');
+// var currentNumHarvesters = Game.creeps.length;
+// console.log(currentNumHarvesters);
+//
+// var name = undefined;
+// if (currentNumHarvesters < maxHarvesters) {
+//     name = Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], undefined, {
+//         role: 'harvester',
+//         working: false
+//     });
+// }
+//
+// if (!(name < 0)) {
+//     console.log("! new creep: " + name);
+// }
 
-};
+}
+;
 
