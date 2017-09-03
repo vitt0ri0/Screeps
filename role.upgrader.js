f737b247cba68ebdf8ceb5928e06ad28009bd617
Vitt0ri0 <vityaii@mail.ru>
2017-09-03 21:24:35 +0300

    .

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,6 +1,9 @@
 var roleUpgrader = {
 
         run: function (creep) {
+            if (creep.memory.upgrading == undefined) {
+                creep.memory.upgrading = false;
+            }
             if (creep.memory.upgrading && creep.carry.energy == 0) {
                 creep.memory.upgrading = true;
             } else if (!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
