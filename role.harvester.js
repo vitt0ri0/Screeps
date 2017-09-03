9e5c540ee14fa56332866367b52a4f26a06c7743
Vitt0ri0 <vityaii@mail.ru>
2017-09-03 21:22:33 +0300

    .

--- role.harvester.js
+++ role.harvester.js
@@ -8,7 +8,7 @@ var roleHarvester = {
             // }
             var sources = creep.room.find(FIND_SOURCES);
             if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         } else {
             // if (creep.carry.energy == creep.carryCapacity) {
@@ -16,7 +16,7 @@ var roleHarvester = {
             // }
             if (Game.spawns['Spawn1'].energy < Game.spawns['Spawn1'].energyCapacity) {
                 if (creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(Game.spawns['Spawn1']);
+                    creep.moveTo(Game.spawns['Spawn1'], {visualizePathStyle: {stroke: '#ffaa00'}});
                 }
             }
         }
