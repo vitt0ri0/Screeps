ed27f0e806de10d14c9a1713f430a1bd05b45582
Vitt0ri0 <vityaii@mail.ru>
2017-09-03 22:11:18 +0300

    .

--- role.builder.js
+++ role.builder.js
@@ -16,7 +16,7 @@ var roleBuilder = {
             }
         } else {
             // var sources = creep.room.find(FIND_SOURCES);
-            var source = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
             if (creep.harvest(source) == ERR_NOT_IN_RANGE)
                 creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
--- role.upgrader.js
+++ role.upgrader.js
@@ -17,7 +17,7 @@ var roleUpgrader = {
                 // if (creep.carry.energy == creep.carryCapacity) {
                 //     creep.say('gonna upgrade');
                 // }
-                var source = this.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+                var source = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
                 if (creep.harvest(source) == ERR_NOT_IN_RANGE)
                     creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
